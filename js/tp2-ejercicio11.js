60ec0f5406df154bf936600af3c848e610446c68
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-11-02 20:22:03 -0300

    correcion ejercicio 11 tp2

--- js/tp2-ejercicio11.js
+++ js/tp2-ejercicio11.js
@@ -3,8 +3,8 @@
 const nombres = [];
 const edades = [];
 for(let i=0; i<=2;i++){
-let nombre = nombres.unshift(prompt('Ingrese el nombre:'));
-let edad = edades.unshift(parseInt(prompt('Ingrese la edad:')));
+let nombre = nombres.unshift(prompt('Ingrese el nombre '+ (i+1)));
+let edad = edades.unshift(parseInt(prompt('Ingrese la edad ' + + (i+1))));
 }
 
 const indiceMax = edades.indexOf(Math.max(...edades));

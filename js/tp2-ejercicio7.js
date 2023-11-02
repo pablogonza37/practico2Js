22ed54d988f7aa338189190513470f78271d7703
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-11-02 14:59:25 -0300

    correccion ejercicio 7 tp2

--- js/tp2-ejercicio7.js
+++ js/tp2-ejercicio7.js
@@ -1,8 +1,14 @@
 // Ejercicio 7
 
-for(let i=30; i >= 1; i--){
+let numero = parseInt(prompt('Ingrese un numero del 1 al 50:'));
+
+if (numero >= 1 && numero <= 50){
+for(let i=numero; i >= 1; i--){
     for(let j=1; j<=i; j++){
     document.write(`${i}`);
     }
     document.write(`</br>`);
+}
+}else {
+    alert('Por favor, Ingrese un numero del 1 al 50');
 }

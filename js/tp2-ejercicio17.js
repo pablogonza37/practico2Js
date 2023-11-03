7bd8efc5fed067d7da7fb42e7ae3f49184ce76bc
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-11-03 00:39:49 -0300

    se realizo ejercicio 17 del tp2

---
+++ js/tp2-ejercicio17.js
@@ -0,0 +1,15 @@
+// Ejercicio 17
+
+const texto = prompt("ingrese un texto");
+let indice = 0;
+while (
+  texto.charAt(indice) !== "a" &&
+  texto.charAt(indice) !== "e" &&
+  texto.charAt(indice) !== "i" &&
+  texto.charAt(indice) !== "o" &&
+  texto.charAt(indice) !== "u"
+) {
+  indice++;
+}
+
+document.write(`la vocal ${texto.charAt(indice)} está en la posición ${indice}`);

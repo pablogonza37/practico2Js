6cc9b082e6a51db00900afccfb02c193ac679793
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-11-02 23:06:48 -0300

    se modifico ejercicio16 del tp2

--- js/tp2-ejercicio16.js
+++ js/tp2-ejercicio16.js
@@ -1,10 +1,10 @@
 // Ejercicio 16
 
-let cadenaInvertida = '';
-const cadenaTexto = prompt('Ingrese una cadena de texto');
+let cadenaInvertida = "";
+const cadenaTexto = prompt("Ingrese una cadena de texto");
 
-for (let indice = cadenaTexto.length - 1; indice >= 0; indice--) { 
-    cadenaInvertida += cadenaTexto[indice]; 
+for (let indice = cadenaTexto.length - 1; indice >= 0; indice--) {
+  cadenaInvertida += cadenaTexto[indice];
 }
 
 document.write(`${cadenaInvertida}`);
